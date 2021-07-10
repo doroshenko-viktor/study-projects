524594e43997627f92249dcd75b29867a7b01685
Viktor Doroshenko <reg.doroshenko@gmail.com>
2021-07-11 01:04:08 +0300

    added references

--- csharp/social-media-app/SocialApp.WebApi/Program.cs
+++ csharp/social-media-app/SocialApp.WebApi/Program.cs
@@ -1,10 +1,35 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SocialApp.Application.Commands;
+using SocialApp.Application.Configuration;
+using SocialApp.Infrastructure.Mongo.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.SetupApplication();
+builder.Services.SetupRepositories(options => builder.Configuration.GetSection("MongoSettings").Bind(options));
 
 var app = builder.Build();
+
+app.MapPost("/api/v1/posts", async (
+    [FromServices] IMediator mediator,
+    [FromBody] CreatePostCommand post
+) =>
+{
+    var result = await mediator.Send(post);
+    if (result.Success)
+    {
+        return Results.Ok(result.Post);
+    }
+    if (result.ValidationResult is not null)
+    {
+        return Results.BadRequest(result.ValidationResult);
+    }
+    return Results.BadRequest();
+});
 
 if (app.Environment.IsDevelopment())
 {
