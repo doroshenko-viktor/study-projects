f9cb31a9bb0e53dfe2b0b9796dcf8a75f85a004f
Viktor Doroshenko <reg.doroshenko@gmail.com>
2021-07-31 01:07:43 +0300

    posts repository + config

---
+++ csharp/social-media-app/SocialApp.Infrastructure.Mongo/Configuration/MongoConfigurator.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using SocialApp.Application.Posts.Interfaces.Repositories;
+using SocialApp.Infrastructure.Mongo.Posts;
+using SocialApp.Infrastructure.Mongo.Posts.DAO;
+
+namespace SocialApp.Infrastructure.Mongo.Configuration;
+
+public static class MongoConfigurator
+{
+    public static IServiceCollection SetupRepositories(
+        this IServiceCollection services,
+        Action<MongoDbOptions> configureOptions
+    )
+    {
+        services.Configure(configureOptions);
+
+        services.AddSingleton<IMongoClient>(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<MongoDbOptions>>();
+            return new MongoClient(options.Value.ConnectionString);
+        });
+
+        services.AddSingleton(sp =>
+        {
+            var mongoClient = sp.GetRequiredService<IMongoClient>();
+            return mongoClient.GetDatabase("social-app").GetCollection<PostMongoDAO>("posts");
+        });
+
+        services.AddScoped<IPostsRepository, PostsRepository>();
+
+        return services;
+    }
+}
---
+++ csharp/social-media-app/SocialApp.Infrastructure.Mongo/Posts/PostsRepository.cs
@@ -0,0 +1,23 @@
+using MongoDB.Driver;
+using SocialApp.Application.Posts.Interfaces.Repositories;
+using SocialApp.Domain.Posts.Entities;
+using SocialApp.Infrastructure.Mongo.Posts.DAO;
+using SocialApp.Infrastructure.Mongo.Posts.Mapping;
+
+namespace SocialApp.Infrastructure.Mongo.Posts;
+
+public class PostsRepository : IPostsRepository
+{
+    private readonly IMongoCollection<PostMongoDAO> _collection;
+
+    public PostsRepository(IMongoCollection<PostMongoDAO> collection)
+    {
+        _collection = collection;
+    }
+
+    public async Task CreatePost(PostEntity post)
+    {
+        var postDAO = post.ToDAO();
+        await _collection.InsertOneAsync(postDAO);
+    }
+}
