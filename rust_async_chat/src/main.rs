9c898d3a57d84aedc7fbc9a46c7028a8884ae4c9
Viktor Doroshenko <vdoroshenko@veepee.com>
2022-01-19 20:44:38 +0200

    multiclient chat

--- rust_async_chat/src/main.rs
+++ rust_async_chat/src/main.rs
@@ -1,27 +1,37 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     println!("starting...");
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (tx, _rx) = broadcast::channel::<String>((10));
     loop {
         let (mut socket, _) = listener.accept().await.unwrap();
-
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
         tokio::spawn(async move {
-            let (read, mut write) = socket.split();
-            let mut reader = BufReader::new(read);
+            let (reader, mut writer) = socket.split();
+            let mut reader = BufReader::new(reader);
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    },
+                    result = rx.recv() => {
+                        let message = result.unwrap();
+                        writer.write_all(&message.as_bytes()).await.unwrap();
+                    }
                 }
-                write.write_all(&line.as_bytes()).await.unwrap();
-                line.clear();
             }
         });
     }
