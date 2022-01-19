f4afc650bd4a5ef778c79b74763bee1e8f08e53d
Viktor Doroshenko <vdoroshenko@veepee.com>
2022-01-19 20:59:22 +0200

    removed duplications of self messages

--- rust_async_chat/src/main.rs
+++ rust_async_chat/src/main.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+
 use tokio::{
     io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
@@ -8,9 +10,9 @@ use tokio::{
 async fn main() {
     println!("starting...");
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-    let (tx, _rx) = broadcast::channel::<String>((10));
+    let (tx, _rx) = broadcast::channel::<(String, SocketAddr)>((10));
     loop {
-        let (mut socket, _) = listener.accept().await.unwrap();
+        let (mut socket, connection_addr) = listener.accept().await.unwrap();
         let tx = tx.clone();
         let mut rx = tx.subscribe();
         tokio::spawn(async move {
@@ -24,12 +26,14 @@ async fn main() {
                         if result.unwrap() == 0 {
                             break;
                         }
-                        tx.send(line.clone()).unwrap();
+                        tx.send((line.clone(), connection_addr.clone())).unwrap();
                         line.clear();
                     },
                     result = rx.recv() => {
-                        let message = result.unwrap();
-                        writer.write_all(&message.as_bytes()).await.unwrap();
+                        let (message, sender_addr) = result.unwrap();
+                        if sender_addr != connection_addr {
+                            writer.write_all(&message.as_bytes()).await.unwrap();
+                        }
                     }
                 }
             }
