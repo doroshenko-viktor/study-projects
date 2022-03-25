78acbac3d88c13bcfe779bdc9ae1689a253613cc
Viktor Doroshenko <reg.doroshenko@gmail.com>
2022-03-25 17:40:45 +0300

    js dom

---
+++ js-dom/app.js
@@ -0,0 +1,19 @@
+const $titleById = document.getElementById("main-title");
+const $listByQuery = document.querySelector("ul");
+const $listElements = $listByQuery.getElementsByTagName("li");
+const $firstListElem = document.querySelector("ul li:first-child");
+const $input = document.getElementById("text-input");
+
+$titleById.classList.add('red');
+$titleById.style.fontStyle = "italic";
+
+console.dir($titleById);
+console.dir($listByQuery);
+console.dir($listElements);
+console.log($firstListElem);
+
+const inputValue = $input.getAttribute("value");
+console.log(inputValue);
+
+$input.setAttribute("value", "new text");
+
