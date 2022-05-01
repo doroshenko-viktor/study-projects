49d9965faf4371020e2846dfcb359c953b9b1363
Viktor Doroshenko <reg.doroshenko@gmail.com>
2022-05-01 18:28:55 +0300

    drag drop example

---
+++ js/drag-and-drop/example/script.js
@@ -0,0 +1,35 @@
+const $item = document.getElementById('item');
+const $boxes = document.getElementsByClassName('box');
+console.dir($boxes);
+
+$item.addEventListener('dragstart', (event) => {
+    console.dir(event);
+    event.dataTransfer.setData('text/plain', event.target.id);
+    setTimeout(() => event.target.classList.add('hide'), 0);
+});
+
+$item.addEventListener('dragend', (event) => {
+    console.dir(event);
+    event.target.classList.remove('hide');
+})
+
+for (const box of $boxes) {
+    box.addEventListener('dragenter', (event) => {
+        event.preventDefault();
+        event.target.classList.add('drag-over');
+    });
+    box.addEventListener('dragover', (event) => {
+        event.preventDefault();
+        event.target.classList.add('drag-over');
+    });
+    box.addEventListener('dragleave', (event) => {
+        event.target.classList.remove('drag-over');
+    });
+    box.addEventListener('drop', (event) => {
+        event.target.classList.remove('drag-over');
+        const id = event.dataTransfer.getData('text/plain');
+        const item = document.getElementById(id);
+        event.target.appendChild(item);
+        item.classList.remove('hide');
+    });
+}
