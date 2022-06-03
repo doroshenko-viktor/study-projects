a217cebb758edb53c788807f497777e29000507a
Viktor Doroshenko <reg.doroshenko@gmail.com>
2022-06-04 01:31:37 +0300

    async server

--- rust/async-server/src/main.rs
+++ rust/async-server/src/main.rs
@@ -1,21 +1,27 @@
+use async_std::net::{TcpListener, TcpStream};
+use async_std::{prelude::*, task};
+use futures::StreamExt;
 use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::time::Duration;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+#[async_std::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }
+    listener
+        .incoming()
+        .for_each_concurrent(None, move |stream| async {
+            let stream = stream.unwrap();
+            task::spawn(handle_connection(stream));
+        })
+        .await;
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
+    async_std::task::sleep(Duration::from_secs(2)).await;
     let get = b"GET / HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
@@ -26,6 +32,6 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
 
     let response = format!("{status_line}{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
